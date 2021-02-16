e6ac9d5c27fbda63f86d095a6594baefd620bb4b
Manten Devillé <manten.d@hotmail.com>
2021-02-16 14:07:42 +0100

    Rename classNames

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,15 +3,12 @@ import React from "react";
 const TodoList = () => {
   return (
     <div>
-      <ul className="formOptions">
+      <ul className="fullTodoList">
         <li>
-          {" "}
           <input type="checkbox" name="todoListItem" id="todoItem1" /> Todo 1
         </li>
         <li>
-          {" "}
-          <input type="checkbox" name="todoListItem" id="todoItem2" />
-          Todo 2
+          <input type="checkbox" name="todoListItem" id="todoItem2" /> Todo 2
         </li>
       </ul>
     </div>
