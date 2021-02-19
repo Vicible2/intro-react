65700413966a20dcba00591c41fe6f55be5d9616
Manten Devillé <manten.d@hotmail.com>
2021-02-19 11:30:54 +0100

    Add deleteHandler, working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-const Todo = ({ text, todos, setTodos }) => {
+const Todo = ({ text, todo, todos, setTodos }) => {
   //Events
-  const deleteHandler = () => {};
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id != todo.id));
+    console.log(todo);
+  };
   return (
     <div className="todo">
       <li className="todo-item">
         <input type="checkbox" name="" id="" />
         <i>{text}</i>
-        <button className="trash-btn">
+        <button onClick={deleteHandler} className="trash-btn">
           <i className="fas fa-trash-alt"></i>
         </button>
       </li>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -10,6 +10,7 @@ const TodoList = ({ todos, setTodos }) => {
           <Todo
             setTodos={setTodos}
             todos={todos}
+            todo={todo}
             key={todo.id}
             text={todo.text}
           />
